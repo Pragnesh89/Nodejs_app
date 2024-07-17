5a674d72808d5639f913db30f334d9f8f91b8900
Pragnesh89 <pragnesh@itoneclick.com>
2024-07-17 11:01:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 // index.js
 
+//const express = require('express');
+//const app = express();
+//const port = 3000;
+
+//app.get('/', (req, res) => {
+//  res.send('Hello World!');
+//});
+
+//app.listen(port, () => {
+//  console.log(`App listening at http://localhost:${port}`);
+//});
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const { user } = require('./mongo');
+
+dotenv.config();
+const port = process.env.APP_PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('This app is running properly');
 });
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
