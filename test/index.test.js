dbf5140f2911c4c6d5768079e2bb1f924decabbd
Pragnesh89 <pragnesh@itoneclick.com>
2024-07-17 10:57:25 +0530

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -18,29 +18,4 @@ describe(`test endpoints`, () => {
     expect(res.status).toBe(200)
     expect(res.text).toBe('This app is running properly')
   });
-
-  test(`Call /getUser endpoint`, async () => {
-    const res = await request(app).get("/getUser/test").send();
-    expect(res.status).toBe(200)
-    expect((res) => {
-      res.body.status = 200;
-    })
-  });
-
-  test(`Call /getUsers endpoint`, async () => {
-    const res = await request(app).get("/getUsers").send();
-    expect(res.status).toBe(200)
-    expect((res) => {
-      res.body.status = 200;
-    })
-  });
-    
-  test(`Call /addUser endpoint`, async () => {
-    const res = await request(app).post("/addUser");
-    expect(res.status).toBe(200)
-    expect((res) => {
-      res.body.status = 200;
-    })
-  });
-
 });
